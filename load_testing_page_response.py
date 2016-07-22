aa188395d40f27ae967992b4158c06714719d88b
Artem Fliunt <auggydioz@gmail.com>
2016-07-22 21:25:24 +0300

    set up the project layout; currently porting LoadTestingPageResponse

---
+++ load_testing_page_response.py
@@ -0,0 +1,15 @@
+
+# Load Testing Page Response
+class LoadTestingPageResponse:
+
+    # Request information
+    __info = []
+
+    # Content
+    __content = None
+
+    # DOMDocument of content
+    __doc = None
+
+    def setInfo(self, info):
+        self.info = info
