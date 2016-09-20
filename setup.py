bb990aeb4480b4b09658709a9d864b7ee66c7852
Richard Friedman <richardfriedman@yahoo.com>
2016-09-20 08:04:04 -0400

    Adding maintainer

--- setup.py
+++ setup.py
@@ -6,6 +6,8 @@ setup(name='RedLine13 Test Harness',
       url='https://github.com/redline13/harness-custom-test-python',
       author='Artem Fliunt',
       author_email='ddnomad@protonmail.com',
+      maintainer='Richard Friedman',
+      maintainer_email='rich@redline13.com',
       license='MIT',
       install_requires=[
           'bs4',
